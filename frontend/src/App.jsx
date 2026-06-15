1e41032dca46a7eb67f827e57e39834e2803e906
swethasiva07 <swethasivamalar@gmail.com>
2026-06-15 14:50:06 +0530

    Remove unused Vite assets

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
@@ -12,8 +12,8 @@ function App() {
       <section id="center">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+          {/* <img src={reactLogo} className="framework" alt="React logo" />
+          <img src={viteLogo} className="vite" alt="Vite logo" /> */}
         </div>
         <div>
           <h1>Kongu Enginerring College</h1>
@@ -40,7 +40,7 @@ function App() {
           <h2>Documentation</h2>
           <p>Your questions, answered</p>
           <ul>
-            <li>
+            {/* <li>
               <a href="https://vite.dev/" target="_blank">
                 <img className="logo" src={viteLogo} alt="" />
                 Explore Vite
@@ -51,7 +51,7 @@ function App() {
                 <img className="button-icon" src={reactLogo} alt="" />
                 Learn more
               </a>
-            </li>
+            </li> */}
           </ul>
         </div>
         <div id="social">
