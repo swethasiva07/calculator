e4b893669624926192cf4dee420f75196fbeb10d
swethasiva07 <swethasivamalar@gmail.com>
2026-06-17 11:47:33 +0530

    Added start script

---
+++ backend/config/db.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const connectDB = async () => {
+    try{
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("MongoDB Connected"+process.env.PORT);
+    }catch(error){
+        console.log(error);
+    }
+};
+module.exports=connectDB;
